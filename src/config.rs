9bd4556e516a5504713ac6926a01443f7abbd664
xiaoabiao <36405417+xiaoabiao@users.noreply.github.com>
2025-12-05 10:40:34 +0800

    更新服务器ip和固定密码
    1

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["202.168.170.175"];
+pub const RS_PUB_KEY: &str = "0pJrELTKNBbv+DfrTl79n6lv3ASYHv5P8GOB+Ek0To8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
